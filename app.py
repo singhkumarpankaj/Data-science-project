c812a971dbca0b0c448fd89c6dc019ce722797ea
singhkumarpankaj <pk8432812@gmail.com>
2025-04-11 13:56:28 +0530

    Remove submodule and added files

---
+++ app.py
@@ -0,0 +1,29 @@
+from src.Data_Science_project.logger import logging
+from src.Data_Science_project.exception import CustomException
+from src.Data_Science_project.components.data_ingestion import DataIngestion
+from src.Data_Science_project.components.data_ingestion import DataIngestionConfig
+from src.Data_Science_project.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.Data_Science_project.components.model_trainer import ModelTrainerConfig,ModelTrainer
+import sys
+
+
+
+if __name__=="__main__":
+    logging.info("The execution has started")
+
+    try:
+       # data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+      #  data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        ## Model Training
+        model_trainer=ModelTrainer()
+        model_trainer.initiate_model_trainer(train_arr,test_arr)
+
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
